eb6a54dd2f76707ec1e1380b6d192b71628d9e5f
winiyaz <winiyaz@proton.me>
2024-09-14 10:52:30 -0700

    👌WomanAssLick

--- LESONS/L9/L9.py
+++ LESONS/L9/L9.py
@@ -1,8 +1,8 @@
 # Lesson 9 - Frames and Labels with TTKBootStrap
 
 # --- Imports ---
-from tkinter import *
 import ttkbootstrap as tb
+from tkinter import *
 
 # --- Imports ---
 
@@ -11,12 +11,27 @@ root = tb.Window(themename="superhero")
 
 # Window Setup
 root.title("Lesson 9 Frames and Labels")
-root.geometry("800x800")
+root.geometry("800x600")
 
 # Main Label
 main_label = tb.Label(text="Lesson 9 Frames and Labels", font=("Helvetica", 30))
 main_label.pack(pady="20")
 
+def Thing():
+	pass
+
+# Frame
+my_frame1 = tb.Frame(root, bootstyle="light")
+my_frame1.pack(pady=40)
+
+my_entry1 = tb.Entry(my_frame1, font=('helvetica', 20))
+my_entry1.pack(pady=20, padx=20)
+
+my_button1 = tb.Button(my_frame1, text="ClickMe", bootstyle="dark", command=Thing)
+my_button1.pack(pady=20, padx=20)
+
+my_label1 = tb.Label(root, text="HeyHey", font=('Arial', 20), bootstyle=" inverse light")
+my_label1.pack(pady=20)
 
 
 # --- Window Run Loop ---

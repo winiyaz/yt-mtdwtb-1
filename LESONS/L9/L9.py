9244d69e299ad4bee3f6054913615510dbe774fa
winiyaz <winiyaz@proton.me>
2024-09-14 10:41:35 -0700

    👌WomanAssLick

---
+++ LESONS/L9/L9.py
@@ -0,0 +1,23 @@
+# Lesson 9 - Frames and Labels with TTKBootStrap
+
+# --- Imports ---
+from tkinter import *
+import ttkbootstrap as tb
+
+# --- Imports ---
+
+# Theme setup
+root = tb.Window(themename="solar")
+
+# Window Setup
+root.title("Lesson 9 Frames and Labels")
+root.geometry("800x800")
+
+# Main Label
+main_label = tb.Label(text="Lesson 9 Frames and Labels", font=("Helvetica", 30))
+main_label.pack(pady="20")
+
+
+
+# --- Window Run Loop ---
+root.mainloop()

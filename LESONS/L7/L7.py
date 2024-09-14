d5908478169adfc46bcb2562a102d704a59688cb
winiyaz <winiyaz@proton.me>
2024-09-14 07:24:26 -0700

    👌WomanAssLick

--- LESONS/L7/L7.py
+++ LESONS/L7/L7.py
@@ -18,41 +18,46 @@ root.geometry("800x800")
 main_label = tb.Label(text="L7FloodGateAndProgressBar", font=("Helvetica", 30))
 main_label.pack(pady="20")
 
-# --- Main Code here ---
-
 
 # --- FloodGuageCode ---
 
-#Functiond
+# Functiond
 
 def starter():
-	pass
+	""" start gauge"""
+	my_gauge.start()
+
 
 def stopper():
-	pass
+	""" stop gauge"""
+	my_gauge.stop()
+
 
 def incrementer():
-	pass
-
+	"""Incrementer"""
+	my_gauge.step(20)
+	label2.config(text=f"Position: {my_gauge.variable.get()}")
 
 
 my_gauge = tb.Floodgauge(root, bootstyle="success", font=('Helvetica', 18),
-						 mask="Pos: {}%", maximum=80, orient="horizontal", value=10
+						 mask="Pos: {}%", maximum=80, orient="horizontal", value=10,
 						 )
 my_gauge.pack(pady=20, fill=X, padx=20)
-
 
 my_button_style = tb.Style()
 my_button_style.configure('danger.TButton', font=("Helvetica", 30))
 
-start_button = tb.Button(root, text="Start", bootstyle="danger outline", command=starter, width=40)
+start_button = tb.Button(root, text="Start", style="danger.TButton", command=starter, width=20)
 start_button.pack(pady=20)
 
-stop_button = tb.Button(root, text="STOP", bootstyle="danger outline", command=stopper, width=40)
+stop_button = tb.Button(root, text="STOP", style="danger.TButton", command=stopper, width=20)
 stop_button.pack(pady=20)
 
-inc_button = tb.Button(root, text="INCR", bootstyle="danger outline", command=incrementer, width=40)
+inc_button = tb.Button(root, text="INCR", style="danger.TButton", command=incrementer, width=20)
 inc_button.pack(pady=20)
+
+label2 = tb.Label(root, text="Position: ")
+label2.pack(pady=20)
 
 # --- Window Run Loop ---
 root.mainloop()

15b18dcd528dd04a34a20d9ccc861b87641a895b
winiyaz <winiyaz@proton.me>
2024-09-14 10:14:04 -0700

    👌WomanAssLick

--- LESONS/L8/L8.py
+++ LESONS/L8/L8.py
@@ -1,7 +1,4 @@
-# Date Picker
-
 # --- Imports ---
-from tkinter import *
 import ttkbootstrap as tb
 
 # --- Imports ---
@@ -16,6 +13,25 @@ root.geometry("800x800")
 # Main Label
 main_label = tb.Label(text="L8 Date Picker", font=("Helvetica", 30))
 main_label.pack(pady="20")
+
+
+# Date entry widget - Calendar Widget
+
+def Datey():
+	# Grab Date
+	my_date_label.config(text=f"Piked = {my_date.entry.get()}")
+
+
+my_date = tb.DateEntry(root, bootstyle="info")
+my_date.pack(pady=50)
+
+my_date_button_style = tb.Style()
+my_date_button_style.configure('danger.Outline.TButton', font=("Helvetica", 20))
+my_date_button = tb.Button(root, text="GetPanty", style="danger.Outline.TButton", command=Datey)
+my_date_button.pack(pady=50)
+
+my_date_label = tb.Label(root, text="Picked-->", font=('Helvetica', 30))
+my_date_label.pack(pady=50)
 
 # --- Window Run Loop ---
 root.mainloop()

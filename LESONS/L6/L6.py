f9b66ffd7d7625454b6b88d4c8f3fe6a090e7ac4
winiyaz <winiyaz@proton.me>
2024-09-14 06:42:39 -0700

    👌WomanAssLick

--- LESONS/L6/L6.py
+++ LESONS/L6/L6.py
@@ -1,7 +1,6 @@
 # Lesson6 - Entry Widgets
 
 # --- Imports ---
-from tkinter import *
 import ttkbootstrap as tb
 
 # --- Imports ---
@@ -17,12 +16,14 @@ root.geometry("800x600")
 main_label = tb.Label(text="L6 EntryBoxes", font=("Helvetica", 30))
 main_label.pack(pady="20")
 
+
 # Create Entry Button
 def speak():
 	my_entry_label.config(text=f"Typed: {my_entry.get()}")
 
+
 # Entry boxes
-my_entry = tb.Entry(root, font=("Helvetica", 20), width=50)
+my_entry = tb.Entry(root, font=("Helvetica", 20), width=50, show="❤️")
 my_entry.pack(pady=50)
 
 # Entry Button
@@ -34,8 +35,6 @@ my_entry_button.pack(pady=20)
 
 my_entry_label = tb.Label(root, text="", font=("Helvetica", 30))
 my_entry_label.pack(pady=20)
-
-
 
 # --- Window Run Loop ---
 root.mainloop()
